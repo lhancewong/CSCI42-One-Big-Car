d0b1e9ed82ef16e4369bfcb44fa547b59f780b99
Anya Habana <anya.habana@gmail.com>
2022-10-07 16:22:50 +0800

    Added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:one_big_car/login_page.dart';
 import 'package:one_big_car/user_profile.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
