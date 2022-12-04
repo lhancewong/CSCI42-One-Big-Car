cc763e89fdca72a46f337af746ca1b091472e373
Lhance Wong <lhance.wong@obf.ateneo.edu>
2022-12-04 23:44:01 +0800

    Revert user_profile to commit 0168493 version

--- lib/pages/user/user_profile.dart
+++ lib/pages/user/user_profile.dart
@@ -53,12 +53,8 @@ class _UserProfileState extends State<UserProfile> {
     Fluttertoast.showToast(msg: "Profile information has been saved.");
     Navigator.pushNamed(context, '/Homepage', arguments: <String, String>{
       "name": name,
+      "role": dropdownValue.toString(),
     });
-
-    String getData() {
-      final User user = fAuth.currentUser!;
-      return user.displayName ?? 'Anon User';
-    }
   }
 
   void pickUploadProfilePicture() async {
@@ -112,12 +108,19 @@ class _UserProfileState extends State<UserProfile> {
               side: BorderSide(color: obcGrey))),
     );
 
+    ButtonStyle buttonStyle2 = ButtonStyle(
+      padding:
+          const MaterialStatePropertyAll(EdgeInsets.fromLTRB(10, 15, 10, 15)),
+      backgroundColor: MaterialStatePropertyAll<Color>(obcGrey),
+      foregroundColor: const MaterialStatePropertyAll<Color>(Colors.black),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(13),
+              side: BorderSide(color: obcGrey))),
+    );
+
     return Scaffold(
       backgroundColor: Colors.white,
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      floatingActionButton: BackButton(
-        color: obcBlue,
-      ),
       body: Stack(
         children: [
           Align(
@@ -197,6 +200,12 @@ class _UserProfileState extends State<UserProfile> {
                       borderRadius: const BorderRadius.all(Radius.circular(16)),
                       color: Colors.white),
                   child: DropdownButton<String>(
+                    hint: const Text("Choose Role",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 24,
+                          color: Colors.grey,
+                        )),
                     value: dropdownValue,
                     underline: Container(color: Colors.white),
                     isExpanded: true,
@@ -225,18 +234,47 @@ class _UserProfileState extends State<UserProfile> {
                   ),
                 ),
                 const SizedBox(height: 50),
-                ElevatedButton(
-                  style: buttonStyle,
-                  child: Text("Save",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 26,
-                        color: obcBlue,
-                      )),
-                  onPressed: () {
-                    saveInfo();
-                  },
-                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: buttonStyle2,
+                        child: Text("Home",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 26,
+                              color: obcBlue,
+                            )),
+                        onPressed: () {
+                          if (dropdownValue == null) {
+                            Fluttertoast.showToast(
+                                msg:
+                                    "Please choose a role before proceeding to Home.");
+                          } else {
+                            saveInfo();
+                          }
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: buttonStyle2,
+                        child: Text("Sign-out",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 26,
+                              color: obcBlue,
+                            )),
+                        onPressed: () {
+                          fAuth.signOut();
+                          Navigator.of(context).pushNamed('/');
+                        },
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
